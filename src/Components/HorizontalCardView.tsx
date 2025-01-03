1681a98ded2f0c181510e7521e931256b8c85a29
Banti Kumar <mr.bantikumar9716@gmail.com>
2025-01-03 22:53:57 +0530

    handle card css

--- src/Components/HorizontalCardView.tsx
+++ src/Components/HorizontalCardView.tsx
@@ -1,21 +1,35 @@
 import React from "react";
 
-const HorizontalCardList = ({ data }:any) => {
+const HorizontalCardList = ({ data }: any) => {
   return (
     <div className="horizontal-card-list-container">
       <div className="horizontal-card-list">
-        {data.map((item:any) => (
-          <div key={item[0].product_id} className="card"> 
-          <div className="flex justify-center">
-            <img src={item[0].image_url} alt={item[0].name} className="card-image" width={140}/>
+        {data.map((item: any) => (
+          <div key={item[0].product_id} className="card">
+            <div className="card-content"></div>
+            <div className="flex justify-center">
+              <img src={item[0].image_url} alt={item[0].name} className="card-image" width={140} />
             </div>
-            <div className="card-details">
-              <h3 className="card-title">{item[0].name}</h3>
-              <p className="card-brand">Brand: {item[0].brand}</p>
-              <p className="card-price">Price: ₹{item[0].price}</p>
-              {item[0].offer && <p className="card-offer">{item[0].offer}</p>}
-              <p className="card-unit">Unit: {item[0].unit}</p>
-              <p className="card-eta">ETA: {item[0].eta_tag.text}</p>
+            <div className="card-details border">
+              <div className="card-eta">
+                <p className="card-eta">{item[0].eta_tag.text}</p>
+              </div>
+              <div className="card-titles">
+                <div className="card-title">
+                  <h3 className="card-title">{item[0].name}</h3>
+                </div>
+                <div>
+                  <p className="card-unit">Unit: {item[0].unit}</p>
+                </div>
+              </div>
+              <div className="flex justify-between text-center items-center">
+                <p className="card-price">₹{item[0].price}</p>
+                <div className="">
+                  <button className="btn px-5 py-[6px] bg-green-50 border-[1px] text-sm border-green-700 rounded-md text-green-700 font-semibold">ADD</button>
+                </div>
+                {item[0].offer && <p className="card-offer">{item[0].offer}</p>}
+              </div>
+
             </div>
           </div>
         ))}
